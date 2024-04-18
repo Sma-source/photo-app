56f5d9655d4845b48fe4741e4414b058a0559229
Sma-source <rabhi.smain@gmail.com>
2024-04-18 14:25:39 +0200

    create new state for storing checkinf if user is new or not

--- components/AuthForm.jsx
+++ components/AuthForm.jsx
@@ -10,8 +10,10 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
 const AuthForm = () => {
+  const [isNewUser, setIsNewUser] = useState(false);
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
