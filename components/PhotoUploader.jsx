8a91f5bd19a566b217645eca18eb8a5d43650533
Sma-source <rabhi.smain@gmail.com>
2024-04-19 17:09:10 +0200

    add inut upload ui

--- components/PhotoUploader.jsx
+++ components/PhotoUploader.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const PhotoUploader = () => {
-  return <div>PhotoUploader</div>;
+  return (
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="picture">Picture</Label>
+      <Input
+        id="picture"
+        type="file"
+        className="text-primary cursor-pointer file:border file:border-solid file:border-black file:rounded-md"
+      />
+    </div>
+  );
 };
 
 export default PhotoUploader;
