d9c365c636eb67379fa9352276eb6ac839280b50
Sma-source <rabhi.smain@gmail.com>
2024-04-19 22:24:41 +0200

    create function to fetch photo and get photo url thn display them via maping a photo component

--- components/PhotoGrid.jsx
+++ components/PhotoGrid.jsx
@@ -1,8 +1,37 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-const PhotoGrid = () => {
+const fetchUserPhotos = async (user, supabaseServer) => {
+  if (!user) return;
+  const folderPath = `user_uploads/${user.id}/`;
+  const { data, error } = await supabaseServer.storage
+    .from("photos")
+    .list(folderPath);
+
+  if (error) {
+    console.error("Error fetching photos", error);
+    return;
+  }
+  return data;
+};
+
+const getPhotoUrls = async (photos, user, supabaseServer) => {
+  return Promise.all(
+    photos.map(async (photo) => {
+      const { data, error } = await supabaseServer.storage
+        .from("photos")
+        .createSignedUrl(`user_uploads/${user.id}/${photo.name}`, 60 * 60);
+      if (error) {
+        console.error("Error generating signed url", error);
+        return null;
+      }
+      return { url: data.signedUrl, photoName: photo.name };
+    })
+  );
+};
+const PhotoGrid = async () => {
   const cookieStore = cookies();
+
   const supabaseServer = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
@@ -14,7 +43,26 @@ const PhotoGrid = () => {
       },
     }
   );
-  return <div>PhotoGrid</div>;
+
+  const {
+    data: { user },
+  } = await supabaseServer.auth.getUser();
+  const photos = await fetchUserPhotos(user, supabaseServer);
+  const photoObjects = await getPhotoUrls(photos, user, supabaseServer);
+  return (
+    <div className="flex flex-wrap justify-center gap-4">
+      {displayedPhotos.map((photo) => (
+        <Photo
+          key={photo.photoName}
+          src={photo.url}
+          alt={`Photo ${photo.photoName}`}
+          width={200}
+          height={200}
+          photoName={photo.photoName}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default PhotoGrid;
