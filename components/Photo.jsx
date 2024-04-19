8a04c03761f0b782fa70715ba0a5f5e7a0822eac
Sma-source <rabhi.smain@gmail.com>
2024-04-19 23:01:42 +0200

    display image

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,7 +1,23 @@
 "use client";
+import Image from "next/image";
 
-const Photo = () => {
-  return <div>Photo</div>;
+const Photo = ({ src, alt, width, height, photoName }) => {
+  return (
+    <>
+      <div
+        style={{ width, height }}
+        className="relative w-auto h-auto shadow-md border border-white border-opacity-80 rounded-lg overflow-hidden cursor-pointer"
+      >
+        <Image
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          style={{ objectFit: "cover", objectPosition: "center" }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Photo;
