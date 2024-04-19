f562b52e80645546e467761c151cbfada28790bc
Sma-source <rabhi.smain@gmail.com>
2024-04-19 17:42:13 +0200

    create function to handle upload photo to superbase storage

--- components/PhotoUploader.jsx
+++ components/PhotoUploader.jsx
@@ -1,10 +1,51 @@
 "use client";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { supabase } from "@/utils/supabaseClient";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const PhotoUploader = () => {
   const [uploading, setUploading] = useState(false);
+  const router = useRouter();
+
+  const handleFileUpload = async (event) => {
+    try {
+      setUploading(true);
+      const file = event.target.files[0];
+      const fileExt = file.name.split(".").pop();
+      const fileName = `${Math.random()}.${fileExt}`;
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      if (!user) {
+        throw new Error("User not authenticated for upload photo");
+      }
+
+      const filePath = `user_uploads/${user.id}/${fileName}`;
+      const { error } = await supabase.storage
+        .from("photos")
+        .upload(filePath, file);
+
+      if (error) {
+        throw error;
+      }
+
+      await fetch("/api/revalidate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ path: "/photos" }),
+      });
+
+      router.refresh();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setUploading(false);
+    }
+  };
   return (
     <div className="grid w-full max-w-sm items-center gap-1.5">
       <Label htmlFor="picture">
@@ -14,6 +55,7 @@ const PhotoUploader = () => {
         id="picture"
         type="file"
         className="text-primary cursor-pointer file:border file:border-solid file:border-black file:rounded-md"
+        onChange={handleFileUpload}
         disabled={uploading}
       />
     </div>
