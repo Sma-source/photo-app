7970a36d52d5ee19de551b38417fe236347e3183
Sma-source <rabhi.smain@gmail.com>
2024-04-20 18:24:20 +0200

    fix error: using get.user before getSession to avoid security risk

--- app/auth/signout/route.js
+++ app/auth/signout/route.js
@@ -22,14 +22,29 @@ export async function POST(req) {
       },
     }
   );
+  // Retrieve user information first
+  const { data: user } = await supabase.auth.getUser();
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+  // If user is authenticated
+  if (user) {
+    // Retrieve session information
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
 
-  if (session) {
-    await supabase.auth.signOut();
+    // If session exists, sign out the user
+    if (session) {
+      await supabase.auth.signOut();
+    }
   }
+
+  // const {
+  //   data: { session },
+  // } = await supabase.auth.getSession();
+
+  // if (session) {
+  //   await supabase.auth.signOut();
+  // }
 
   return NextResponse.redirect(new URL("/", req.url), {
     status: 302,
