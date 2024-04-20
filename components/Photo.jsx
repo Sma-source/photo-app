6324c88654d65bfe3af4bc54a60084f018af2d4f
Sma-source <rabhi.smain@gmail.com>
2024-04-20 17:53:58 +0200

    add trash icon ui

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -2,6 +2,15 @@
 import Image from "next/image";
 import { useState } from "react";
 import PhotoModal from "./PhotoModal";
+import { SquareX, Trash2 } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Photo = ({ src, alt, width, height, photoName }) => {
   const [showModal, setShowModal] = useState(false);
@@ -14,6 +23,15 @@ const Photo = ({ src, alt, width, height, photoName }) => {
         style={{ width, height }}
         className="relative w-auto h-auto shadow-md border border-white border-opacity-80 rounded-lg overflow-hidden cursor-pointer"
       >
+        <form action="" className="absolute bottom-1 right-1 z-10">
+          <input type="hidden" name="photoPath" value={src} />
+          <button
+            type="submit"
+            className="bg-transparent border-none text-red-500 cursor-pointer hover:text-red-500 hover:scale-110 transition duration-300"
+          >
+            <Trash2 className="w-6 h-6" />
+          </button>
+        </form>
         <Image
           src={src}
           alt={alt}
