635be0980b8602989b27700c20b6dc545a19136c
Sma-source <rabhi.smain@gmail.com>
2024-04-20 16:38:09 +0200

    add ui for modal

--- components/PhotoModal.jsx
+++ components/PhotoModal.jsx
@@ -1,7 +1,29 @@
 "use client";
 
+import Image from "next/image";
+
 const PhotoModal = ({ src, alt, onClose }) => {
-  return <div>PhotoModal</div>;
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50">
+      <div className="bg-gray-800 p-4 rounded-lg relative border border-gray-600">
+        <button
+          onClick={onClose}
+          className="text-gray-300 hover:text-white mb-2"
+        >
+          Close
+        </button>
+        <div className="relative w-[80vw] h-[80vh]">
+          <Image
+            src={src}
+            alt={alt}
+            fill={true}
+            style={{ objectFit: "cover", objectPosition: "center" }}
+            className="rounded-lg"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoModal;
