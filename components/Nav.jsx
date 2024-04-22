1e1d456ef6044df4540c5aebc0cfb053455921da
Sma-source <rabhi.smain@gmail.com>
2024-04-22 16:14:58 +0200

    add link & ui

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Link from "next/link";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="fixed top-0 left-0 p-4 z-10">
+      <ul className="flex space-x-4">
+        <li>
+          <Link href="/photos" className="text-gray-300 hover:text-gray-100">
+            Photos
+          </Link>
+        </li>
+        <li>
+          <Link href="/favorites" className="text-gray-300 hover:text-gray-100">
+            Favorites
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
