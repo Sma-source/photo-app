83c59ee8b8b686a0814ca53d7ddafb51c976bc27
Sma-source <rabhi.smain@gmail.com>
2024-04-22 16:22:26 +0200

    import Nav component

--- app/favorites/page.jsx
+++ app/favorites/page.jsx
@@ -1,3 +1,4 @@
+import Nav from "@/components/Nav";
 import PhotoGrid from "@/components/PhotoGrid";
 import SignOutButton from "@/components/SignOutButton";
 import React from "react";
@@ -5,6 +6,7 @@ import React from "react";
 const Favorites = () => {
   return (
     <main className="min-h-screen bg-gray-800 text-white relative p-10">
+      <Nav />
       <div className="container mx-auto px-4 py-4">
         <div className="flex flex-col items-center mb-6">
           <h1 className="text-4xl font-bold mb-4">Favorites</h1>
--- app/photos/page.jsx
+++ app/photos/page.jsx
@@ -1,3 +1,4 @@
+import Nav from "@/components/Nav";
 import PhotoGrid from "@/components/PhotoGrid";
 import PhotoUploader from "@/components/PhotoUploader";
 import SignOutButton from "@/components/SignOutButton";
@@ -6,7 +7,7 @@ import React from "react";
 const Photos = () => {
   return (
     <main className="min-h-screen bg-gray-800 text-white relative p-10">
-      {/* <Nav /> */}
+      <Nav />
       <div className="container mx-auto px-4 py-4">
         <div className="flex flex-col items-center mb-6">
           <h1 className="text-4xl font-bold mb-4">Photos</h1>
