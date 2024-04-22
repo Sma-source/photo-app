1ad82f580f7842491d612815c9aaf6bc4bd5a81c
Sma-source <rabhi.smain@gmail.com>
2024-04-22 16:59:07 +0200

    change bg color

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,8 @@ import AuthForm from "@/components/AuthForm";
 export default function Home() {
   return (
     <main className="flex items-center justify-center bg-gray-900 min-h-screen">
-      <div className="bg-gray-700 rounded-lg shadow-lg p-6 w-full max-w-lg">
-        <h2 className="text-white text-2xl font-bold mb-4 text-center">
+      <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-lg">
+        <h2 className="text-2xl font-bold mb-4 text-center">
           Welcome to Photo App
         </h2>
         <p className="mb-6 text-lg text-center">
